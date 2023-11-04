d2419bb5f6ebb07d1bea611d6772e9999d72932f
Vanessa <millanv185@gmail.com>
2023-11-03 22:11:26 -0700

    Login y registro con BD

--- app/src/main/java/com/example/recyclerview/DeveloperuBD.java
+++ app/src/main/java/com/example/recyclerview/DeveloperuBD.java
@@ -1,6 +1,7 @@
 package com.example.recyclerview;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -24,13 +25,30 @@ public class DeveloperuBD extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
     }
 
-
-
+    //AGREGAR USUARIOS
     public void agregarUsuarios(String username, String email, String password, String telefono, String unidad){
         SQLiteDatabase bd=getWritableDatabase();
         if (bd!=null){
             bd.execSQL("INSERT INTO USUARIOS VALUES('"+username+"','"+email+"','"+password+"','"+telefono+"','"+unidad+"')");
             bd.close();
         }
+    }
+
+    //BUSCAR USUARIOS
+    public boolean buscarUsuario(String username, String password) {
+        SQLiteDatabase bd = getReadableDatabase();
+        if (bd != null) {
+            String[] args = new String[]{username, password};
+            Cursor cursor = bd.rawQuery("SELECT * FROM USUARIOS WHERE USUARIO = ? AND CONTRASEÑA = ?", args);
+
+            if (cursor.getCount() > 0) {
+                cursor.close();
+                return true; // Usuario y contraseña coinciden
+            } else {
+                cursor.close();
+                return false; // Usuario o contraseña incorrecta
+            }
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/recyclerview/activity/MainActivityPrincipal.java
+++ app/src/main/java/com/example/recyclerview/activity/MainActivityPrincipal.java
@@ -6,18 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.recyclerview.DeveloperuBD;
 import com.example.recyclerview.R;
 
 
 public class MainActivityPrincipal extends AppCompatActivity {
 
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private DeveloperuBD db;
     Button Registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_principal);
 
+        //BOTON REGISTRAR
         Registrar = (Button)findViewById(R.id.btn_registrar);
         Registrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +34,32 @@ public class MainActivityPrincipal extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        //BOTON INICIAR SESIÓN
+        etUsername = findViewById(R.id.username);
+        etPassword = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.btn_login);
+        db = new DeveloperuBD(this);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                // Verificar si el usuario y la contraseña coinciden en la base de datos
+                if (db.buscarUsuario(username, password)) {
+                    // Iniciar la actividad MainActivityPrincipal
+                    Intent intent = new Intent(MainActivityPrincipal.this, MainActivity.class);
+                    startActivity(intent);
+                    finish(); // Cierra la actividad de inicio de sesión para que no se pueda volver atrás
+                } else {
+                    Toast.makeText(MainActivityPrincipal.this, "Usuario o contraseña incorrecta", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
 
     }
 
